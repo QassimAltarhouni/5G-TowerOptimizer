90886ab2910cbea37c3ddb741007745b05c11af3
Mohammedkasemaltarhony <86439517+Mohammedkasemaltarhony@users.noreply.github.com>
2025-07-02 07:03:01 +0200

    Done

--- src/.ipynb_checkpoints/data_loader-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_opencellid_data(filepath):
-    columns = [
-        'radio', 'mcc', 'net', 'area', 'cell', 'unit',
-        'lon', 'lat', 'range', 'samples', 'changeable',
-        'created', 'updated', 'averageSignal'
-    ]
-    return pd.read_csv(filepath, header=None, names=columns)
